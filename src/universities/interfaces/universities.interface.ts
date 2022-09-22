28efaed84f7adb2670d125eadda74ad7a104b2ac
diegoandcontroll <lukasxdp@gmail.com>
2022-09-22 16:14:20 -0300

    create universities interface

---
+++ src/universities/interfaces/universities.interface.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export interface Universitie extends Document {
+  readonly alpha_two_code: string;
+  readonly web_pages: [string];
+  readonly name: string;
+  readonly country: string;
+  readonly domains: [string];
+  readonly state_province: null;
+}
--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesService } from './universities.service';
 import { UniversitiesController } from './universities.controller';
+import { universitiesProviders } from './universities.provider';
+import { DatabaseModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UniversitiesController],
-  providers: [UniversitiesService],
+  providers: [UniversitiesService, ...universitiesProviders],
 })
 export class UniversitiesModule {}
