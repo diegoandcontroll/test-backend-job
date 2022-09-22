945850b3b177d12b848e1f3f5320a92c5c0c661d
diegoandcontroll <lukasxdp@gmail.com>
2022-09-22 15:48:23 -0300

    create dbmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UniversitiesModule } from './universities/universities.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UniversitiesModule],
+  imports: [ConfigModule.forRoot(), UniversitiesModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
